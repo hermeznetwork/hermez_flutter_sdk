0349073635bcde12fa80bfb0898d5496b40ce820
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-06-01 16:59:18 +0200

    setup for sdk initial

--- android/src/main/kotlin/io/hermez/hermez_plugin/HermezPlugin.kt
+++ android/src/main/kotlin/io/hermez/hermez_plugin/HermezPlugin.kt
@@ -1,13 +1,11 @@
 package io.hermez.hermez_plugin
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** HermezPlugin */
 class HermezPlugin: FlutterPlugin, MethodCallHandler {
